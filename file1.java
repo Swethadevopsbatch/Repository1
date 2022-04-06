66c778be5605275c7b05960241f3ff248552d483
Sanvi Shaurya <swetha6028@gmail.com>
2022-04-06 19:20:38 -0400

    First java file

---
+++ file1.java
@@ -0,0 +1,12 @@
+This a first Repository
+//Import file class  
+import java.io.File;  
+//Import Scanner class   
+import java.util.Scanner;  
+public class CreateFolder {  
+    //Main() method start  
+    public static void main(String args[]) {        
+      //Using Scanner class to get the path from the user where he wants to create a folder.  
+      System.out.println("Enter the path where you want to create a folder: ");  
+      Scanner sc = new Scanner(System.in);  
+      String path = sc.next();  
