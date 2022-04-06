83c744fdd660a50b49ea6691d062f39e7af69575
Sanvi Shaurya <swetha6028@gmail.com>
2022-04-06 19:24:26 -0400

    second java file

---
+++ file2.java
@@ -0,0 +1,10 @@
+this is second file
+public class Student {
+public static void main(String[] args) {
+int x = 10;
+int y = 12;
+if(x+y > 20) {
+System.out.println("x + y is greater than 20");
+}
+}
+}
